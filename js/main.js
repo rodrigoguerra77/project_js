4a31dbdca2bc8850da8a861af34f1472786c8085
Rodrigo Guerra <rodrigoguerra77@gmail.com>
2020-05-04 15:56:45 -0400

    Incluido slider usando plugin bxslider

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    // Slider
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        pager: false,
+        slideWidth: 1200,
+        auto: true,
+        responsive: true
+    });
+
+});
